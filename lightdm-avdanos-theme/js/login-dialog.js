6047555554f21e2791defd6fbd6255133de75d98
AdvendraDeswanta <adeswanta@gmail.com>
2022-06-23 17:28:10 +0700

    Added some system status functionalities
    * Battery indicator
    * Brightness indicator
    * Keyboard layout indicator
    * Session indicator
    
    Other changes:
    * Updated login form
    * Minor UI changes
    * Added some session symbols/images

--- lightdm-avdanos-theme/js/login-dialog.js
+++ lightdm-avdanos-theme/js/login-dialog.js
@@ -1,11 +1,29 @@
 var lockScreen = document.getElementById("lock-screen");
 var loginScreenContent = document.getElementById("login-content");
+
+var backButton = document.getElementById("back-button");
 var loginButton = document.getElementById("user-input-submit");
+
+var batteryData = document.getElementById("battery-data");
+var brightnessData = document.getElementById("brightness-data");
 
 var LoginDialog = {
     start() {
-        lockScreen.addEventListener("click", this.show);
+        lockScreen.addEventListener("click", Transition.showLogin);
+        backButton.addEventListener("click", Transition.hideLogin);
         loginButton.addEventListener("click", auth.startAuthentication.bind(auth));
+
+        this.updateBatteryData();
+        this.updateBrightnessData();
+
+        lightdm.battery_update.connect(this.updateBatteryData);
+        lightdm.brightness_update.connect(this.updateBrightnessData);
+    },
+    updateBatteryData() {
+        batteryData.innerText = `${lightdm.batteryData.level}%`;
+    },
+    updateBrightnessData() {
+        brightnessData.innerText = `${lightdm.brightness}%`;
     },
     hide() {
         loginScreenContent.classList.add('hide');
