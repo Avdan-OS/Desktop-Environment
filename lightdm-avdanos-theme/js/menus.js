f9722218ef44fb2065661f9eaa9dbbe7dd9b7c3f
AdvendraDeswanta <adeswanta@gmail.com>
2022-06-23 17:24:46 +0700

    Added power menu
    Other changes:
    * Added menu style
    * Added menus to the document
    * Updated some styles

---
+++ lightdm-avdanos-theme/js/menus.js
@@ -0,0 +1,107 @@
+var powerMenu = document.getElementById("power-menu");
+var powerButton = document.getElementById("power-button");
+
+var layoutMenu = document.getElementById("layout-menu");
+var layoutButton = document.getElementById("layout-button");
+var layoutName = document.getElementById("layout-name");
+
+var sessionMenu = document.getElementById("session-menu");
+var sessionButton = document.getElementById("session-button");
+var sessionName = document.getElementById("session-name");
+var sessionIcon = document.getElementById("session-icon");
+
+var Menus = {
+    start() {
+        powerButton.addEventListener('click', () => {
+            this.toggleMenus(powerMenu);
+        });
+        layoutButton.addEventListener('click', () => {
+            this.toggleMenus(layoutMenu);
+        });
+        sessionButton.addEventListener('click', () => {
+            this.toggleMenus(sessionMenu);
+        });
+
+        this._loadLayouts();
+        this._setLayout(
+            lightdm.layouts.find(layout => {
+                return layout.name == lightdm.layout.name;
+            }), false
+        );
+
+        this._loadSessions();
+        this._setSession(
+            lightdm.sessions.find(session => {
+                return session.key == lightdm.default_session;
+            }), false
+        );
+    },
+    closeAllMenus() {
+        powerMenu.classList.remove('show');
+        layoutMenu.classList.remove('show');
+        sessionMenu.classList.remove('show');
+    },
+    toggleMenus(menus) {
+        if (!menus.classList.contains('show')) {
+            this.closeAllMenus();
+            menus.classList.add('show');
+        } else {
+            menus.classList.remove('show');
+        }
+    },
+
+    _loadLayouts() {
+        lightdm.layouts.forEach(layout => {
+            let item = document.createElement('span');
+            item.innerText = `${layout.description} (${layout.name.replace(' ', '_')})`;
+            item.classList.add('item');
+
+            item.addEventListener('click', () => {
+                this.closeAllMenus();
+                this._setLayout(layout);
+            })
+
+            layoutMenu.appendChild(item);
+        })
+    },
+    _setLayout(layout, update = true) {
+        layoutName.innerText = layout.name.replace(' ', '_').toUpperCase();
+
+        if (update) {
+            lightdm.layout = layout;
+        }
+    },
+
+    _loadSessions() {
+        lightdm.sessions.forEach(session => {
+            let item = document.createElement('div');
+            item.classList.add('item');
+
+            let icon = document.createElement('img');
+            icon.classList.add('icon')
+            icon.src = `img/sessions/${session.key}.png`;
+            item.appendChild(icon);
+
+            let name = document.createElement('span');
+            name.innerText = session.name;
+            item.appendChild(name);
+
+            item.addEventListener('click', () => {
+                this.closeAllMenus();
+                this._setSession(session);
+            })
+
+            sessionMenu.appendChild(item);
+        });
+    },
+    _setSession(session, update = true) {
+        sessionName.innerText = session.name;
+        sessionIcon.src = `img/sessions/${session.key}.png`;
+
+        if (update) {
+            lightdm.default_session = session.key;
+        }
+    }
+}
+
+Menus.start();
---
+++ lightdm-avdanos-theme/js/power.js
@@ -0,0 +1,35 @@
+var sleepMenuItem = document.getElementById("sleep-menu-item");
+var hibernateMenuItem = document.getElementById("hibernate-menu-item");
+var restartMenuItem = document.getElementById("restart-menu-item");
+var shutdownMenuItem = document.getElementById("shutdown-menu-item");
+
+var Power = {
+    start() {
+        sleepMenuItem.addEventListener('click', () => {
+            if (lightdm.can_suspend) {
+                Transition.fadeOut();
+                setTimeout(() => { lightdm.suspend(); }, 1000);
+            }
+        });
+        hibernateMenuItem.addEventListener('click', () => {
+            if (lightdm.can_hibernate) {
+                Transition.fadeOut();
+                setTimeout(() => { lightdm.hibernate(); }, 1000);
+            }
+        });
+        restartMenuItem.addEventListener('click', () => {
+            if (lightdm.can_restart) {
+                Transition.fadeOut();
+                setTimeout(() => { lightdm.restart(); }, 1000);
+            }
+        });
+        shutdownMenuItem.addEventListener('click', () => {
+            if (lightdm.can_shutdown) {
+                Transition.fadeOut();
+                setTimeout(() => { lightdm.shutdown(); }, 1000);
+            }
+        });
+    }
+}
+
+Power.start();
